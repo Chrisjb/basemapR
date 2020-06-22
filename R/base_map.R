bb6c3bb83f901106eee9f78c055886891789beab
Liz Roten <liz.roten@metc.state.mn.us>
2020-06-22 11:23:46 -0500

    use styler::style_active_pkg() in tidyverse style

--- R/base_map.R
+++ R/base_map.R
@@ -21,23 +21,20 @@
 #' # add to ggplot
 #' library(ggplot2)
 #' ggplot() +
-#' base_map(bbox, increase_zoom = 2, basemap = 'google-terrain')+
-#' geom_sf(data = localauth_data, fill =NA )  +
-#' coord_sf(xlim = c(bbox$xmin, bbox$xmax), ylim = c(bbox$ymin, bbox$ymax), crs = 4326)
+#'   base_map(bbox, increase_zoom = 2, basemap = "google-terrain") +
+#'   geom_sf(data = localauth_data, fill = NA) +
+#'   coord_sf(xlim = c(bbox$xmin, bbox$xmax), ylim = c(bbox$ymin, bbox$ymax), crs = 4326)
 #'
 #'
 #' # add straight to ggplot
 #'
 #' ggplot() +
-#' base_map(st_bbox(localauth_data), increase_zoom = 2) +
-#' geom_sf(data = localauth_data, fill = NA)
-#'
+#'   base_map(st_bbox(localauth_data), increase_zoom = 2) +
+#'   geom_sf(data = localauth_data, fill = NA)
 #' @export
 
 
-base_map <- function(bbox, increase_zoom=0, basemap = 'dark', nolabels = F){
-
-
+base_map <- function(bbox, increase_zoom = 0, basemap = "dark", nolabels = F) {
   x_len <- bbox["xmax"] - bbox["xmin"]
   y_len <- bbox["ymax"] - bbox["ymin"]
 
@@ -53,8 +50,10 @@ base_map <- function(bbox, increase_zoom=0, basemap = 'dark', nolabels = F){
 
   xy <- lonlat2xy(bbox[c("xmin", "xmax")], bbox[c("ymin", "ymax")], zoom)
 
-  tiles <- expand.grid(x = seq(xy$x["xmin"], xy$x["xmax"]),
-                       y = seq(xy$y["ymin"], xy$y["ymax"]))
+  tiles <- expand.grid(
+    x = seq(xy$x["xmin"], xy$x["xmax"]),
+    y = seq(xy$y["ymin"], xy$y["ymax"])
+  )
 
 
 
@@ -82,87 +81,78 @@ base_map <- function(bbox, increase_zoom=0, basemap = 'dark', nolabels = F){
       url <- paste0("https://tile.openstreetmap.se/hydda/base/", zoom, "/", tiles$x, "/", tiles$y, ".png")
     }
     message('attribution: Tiles courtesy of http://openstreetmap.se/ OpenStreetMap Sweden; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors')
-
-  } else if(basemap == 'voyager') {
-    if(nolabels == F) {
-      url <- paste0('https://basemaps.cartocdn.com/rastertiles/voyager/',zoom,'/',tiles$x,'/',tiles$y,'.png') # voyager
+  } else if (basemap == "voyager") {
+    if (nolabels == F) {
+      url <- paste0("https://basemaps.cartocdn.com/rastertiles/voyager/", zoom, "/", tiles$x, "/", tiles$y, ".png") # voyager
     } else {
-      url <- paste0('https://basemaps.cartocdn.com/rastertiles/voyager_nolabels/',zoom,'/',tiles$x,'/',tiles$y,'.png')
+      url <- paste0("https://basemaps.cartocdn.com/rastertiles/voyager_nolabels/", zoom, "/", tiles$x, "/", tiles$y, ".png")
     }
     message('attribution: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>')
-
-  } else if(basemap == 'france'){
-    if(nolabels == F) {
-      url <- paste0('https://tile.openstreetmap.fr/osmfr/,zoom','/',tiles$x,'/',tiles$y,'.png') # france
+  } else if (basemap == "france") {
+    if (nolabels == F) {
+      url <- paste0("https://tile.openstreetmap.fr/osmfr/,zoom", "/", tiles$x, "/", tiles$y, ".png") # france
     } else {
-      url <- paste0('https://tile.openstreetmap.fr/osmfr/,zoom','/',tiles$x,'/',tiles$y,'.png') # france
-      message('nolabels not available for basemap: ', basemap, '. returning map with labels.')
+      url <- paste0("https://tile.openstreetmap.fr/osmfr/,zoom", "/", tiles$x, "/", tiles$y, ".png") # france
+      message("nolabels not available for basemap: ", basemap, ". returning map with labels.")
     }
     message('attribution: &copy; Openstreetmap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors')
-
-
-  } else if(basemap == 'dark') {
-    if(nolabels == F) {
-      url <- paste0('https://basemaps.cartocdn.com/dark_all/',zoom,'/',tiles$x,'/',tiles$y,'.png') # dark
+  } else if (basemap == "dark") {
+    if (nolabels == F) {
+      url <- paste0("https://basemaps.cartocdn.com/dark_all/", zoom, "/", tiles$x, "/", tiles$y, ".png") # dark
     } else {
-      url <- paste0('https://basemaps.cartocdn.com/dark_nolabels/',zoom,'/',tiles$x,'/',tiles$y,'.png') # dark
+      url <- paste0("https://basemaps.cartocdn.com/dark_nolabels/", zoom, "/", tiles$x, "/", tiles$y, ".png") # dark
     }
     message('attribution: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>')
-
-  } else if(basemap == 'neighbourhood') {
-    if(nolabels == F) {
-      url <- paste0('https://tile.thunderforest.com/neighbourhood/',zoom,'/',tiles$x,'/',tiles$y,'.png') # neighbourhood
+  } else if (basemap == "neighbourhood") {
+    if (nolabels == F) {
+      url <- paste0("https://tile.thunderforest.com/neighbourhood/", zoom, "/", tiles$x, "/", tiles$y, ".png") # neighbourhood
     } else {
-      url <- paste0('https://tile.thunderforest.com/neighbourhood/',zoom,'/',tiles$x,'/',tiles$y,'.png') # neighbourhood
-      message('nolabels not available for basemap: ', basemap, '. returning map with labels.')
+      url <- paste0("https://tile.thunderforest.com/neighbourhood/", zoom, "/", tiles$x, "/", tiles$y, ".png") # neighbourhood
+      message("nolabels not available for basemap: ", basemap, ". returning map with labels.")
     }
     message('attribution: &copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors')
-
-  } else if(basemap == 'mapnik') {
-    url <- paste0('https://tile.openstreetmap.org/',zoom,'/',tiles$x,'/',tiles$y,'.png') #osm mapnik
+  } else if (basemap == "mapnik") {
+    url <- paste0("https://tile.openstreetmap.org/", zoom, "/", tiles$x, "/", tiles$y, ".png") # osm mapnik
     message('attribution: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors')
 
-    if(nolabels == T){
-      message('nolabels not available for basemap: ', basemap, '. returning map with labels.')
+    if (nolabels == T) {
+      message("nolabels not available for basemap: ", basemap, ". returning map with labels.")
     }
-  } else if(basemap == 'wikimedia') {
-    url <- paste0('https://maps.wikimedia.org/osm-intl/',zoom,'/',tiles$x,'/',tiles$y,'.png') #wikimedia
-    message('please see attribution details: https://wikimediafoundation.org/wiki/Maps_Terms_of_Use')
+  } else if (basemap == "wikimedia") {
+    url <- paste0("https://maps.wikimedia.org/osm-intl/", zoom, "/", tiles$x, "/", tiles$y, ".png") # wikimedia
+    message("please see attribution details: https://wikimediafoundation.org/wiki/Maps_Terms_of_Use")
 
-    if(nolabels == T){
-      message('nolabels not available for basemap: ', basemap, '. returning map with labels.')
+    if (nolabels == T) {
+      message("nolabels not available for basemap: ", basemap, ". returning map with labels.")
     }
-
-  } else if(basemap == 'esri') {
-    url <- paste0('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/',zoom,'/',tiles$y,'/',tiles$x,'.png') #esri
-    message('attribution: Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community')
-    if(nolabels == T){
-      message('nolabels not available for basemap: ', basemap, '. returning map with labels.')
+  } else if (basemap == "esri") {
+    url <- paste0("https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/", zoom, "/", tiles$y, "/", tiles$x, ".png") # esri
+    message("attribution: Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community")
+    if (nolabels == T) {
+      message("nolabels not available for basemap: ", basemap, ". returning map with labels.")
     }
-  } else if(basemap == 'esri-imagery') {
-    url <- paste0('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/',zoom,'/',tiles$y,'/',tiles$x,'.png')
+  } else if (basemap == "esri-imagery") {
+    url <- paste0("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/", zoom, "/", tiles$y, "/", tiles$x, ".png")
 
-    message('attribution: Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community')
-  } else if(basemap == 'google'){
-    url <- paste0('https://mt.google.com/vt/lyrs=m&x=',tiles$x,'&y=',tiles$y,'&z=',zoom,'')
-    message('please see attribution details: https://wikimediafoundation.org/wiki/Maps_Terms_of_Use')
-
-  } else if(basemap == 'google-road'){
-    url <- paste0('https://mt.google.com/vt/lyrs=r&x=',tiles$x,'&y=',tiles$y,'&z=',zoom,'')
-    message('please cite: map data \uA9 2020 Google')
-
-  } else if(basemap == 'google-nobg'){
-    url <- paste0('https://mt.google.com/vt/lyrs=h&x=',tiles$x,'&y=',tiles$y,'&z=',zoom,'')
-    message('please cite: map data \uA9 2020 Google')
-  } else if(basemap == 'google-satellite'){
-    url <- paste0('https://mt.google.com/vt/lyrs=s&x=',tiles$x,'&y=',tiles$y,'&z=',zoom,'')
-    message('please cite: map data \uA9 2020 Google')
-  } else if(basemap == 'google-hybrid'){
-    url <- paste0('https://mt.google.com/vt/lyrs=y&x=',tiles$x,'&y=',tiles$y,'&z=',zoom,'')
-    message('please cite: map data \uA9 2020 Google')
-  } else if(basemap == 'google-terrain'){
-    url <- paste0('https://mt.google.com/vt/lyrs=p&x=',tiles$x,'&y=',tiles$y,'&z=',zoom,'')
-    message('please cite: map data \uA9 2020 Google')
+    message("attribution: Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community")
+  } else if (basemap == "google") {
+    url <- paste0("https://mt.google.com/vt/lyrs=m&x=", tiles$x, "&y=", tiles$y, "&z=", zoom, "")
+    message("please see attribution details: https://wikimediafoundation.org/wiki/Maps_Terms_of_Use")
+  } else if (basemap == "google-road") {
+    url <- paste0("https://mt.google.com/vt/lyrs=r&x=", tiles$x, "&y=", tiles$y, "&z=", zoom, "")
+    message("please cite: map data \uA9 2020 Google")
+  } else if (basemap == "google-nobg") {
+    url <- paste0("https://mt.google.com/vt/lyrs=h&x=", tiles$x, "&y=", tiles$y, "&z=", zoom, "")
+    message("please cite: map data \uA9 2020 Google")
+  } else if (basemap == "google-satellite") {
+    url <- paste0("https://mt.google.com/vt/lyrs=s&x=", tiles$x, "&y=", tiles$y, "&z=", zoom, "")
+    message("please cite: map data \uA9 2020 Google")
+  } else if (basemap == "google-hybrid") {
+    url <- paste0("https://mt.google.com/vt/lyrs=y&x=", tiles$x, "&y=", tiles$y, "&z=", zoom, "")
+    message("please cite: map data \uA9 2020 Google")
+  } else if (basemap == "google-terrain") {
+    url <- paste0("https://mt.google.com/vt/lyrs=p&x=", tiles$x, "&y=", tiles$y, "&z=", zoom, "")
+    message("please cite: map data \uA9 2020 Google")
   }
 
 
@@ -173,5 +163,5 @@ base_map <- function(bbox, increase_zoom=0, basemap = 'dark', nolabels = F){
   args <- tile_positions %>%
     dplyr::mutate(raster = pngs)
 
-  return(purrr::pmap(args,annotation_raster, interpolate = TRUE))
+  return(purrr::pmap(args, annotation_raster, interpolate = TRUE))
 }
